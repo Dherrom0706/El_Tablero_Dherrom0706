d1034c8b47198d65f5b0ca7c90dc8577ba06ba49
Dherrom0706 <dionihernandezromera@gmail.com>
2023-01-23 19:44:12 +0100

    revisar colores y numeros no se muestran

--- app/src/main/java/www/iesmurgi/eltablero/GameField.kt
+++ app/src/main/java/www/iesmurgi/eltablero/GameField.kt
@@ -1,8 +1,13 @@
 package www.iesmurgi.eltablero
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.media.MediaPlayer
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
 import android.os.Vibrator
 import android.service.quicksettings.Tile
 import android.util.DisplayMetrics
@@ -28,6 +33,7 @@ class GameField : AppCompatActivity() {
     lateinit var vibratorService: Vibrator
     lateinit var textView: TextView
 
+    @SuppressLint("ServiceCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_field)
@@ -67,8 +73,66 @@ class GameField : AppCompatActivity() {
                 ids[j][i] = ident
                 tv.layoutParams = LinearLayout.LayoutParams(0,height,1.0f)
                 tv.setOnClickListener {
-                    it.x
-                    it.y
+                    var x = it.x
+                    var y = it.y
+                    if (sonido)
+                        mp.start()
+                    if (vibracion){
+                        vibratorService = getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as Vibrator
+                        if (vibratorService.hasVibrator()){
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                                vibratorService.vibrate(
+                                    VibrationEffect.createOneShot(
+                                        100,
+                                        VibrationEffect.DEFAULT_AMPLITUDE
+                                    )
+                                )
+                            }else{
+                                vibratorService.vibrate(100)
+                            }
+                        }
+                    }
+
+                   if (x == 0f && y == 0f){
+                       changeView(0,1)
+                       changeView(1,0)
+                       changeView(1,1)
+                   }else if (x == 0f && y == (topTileY - 1).toFloat()){
+                       changeView(0,topTileY-2)
+                       changeView(1,topTileY-2)
+                       changeView(1,topTileY-1)
+                   }else if (x == (topTileX - 1).toFloat() && y == 0f){
+                       changeView(topTileX-2,0)
+                       changeView(topTileX-2,1)
+                       changeView(topTileX-1,1)
+                   }else if (x == (topTileX - 1).toFloat() && y == (topTileY - 1).toFloat()){
+                       changeView(topTileX-2,topTileY-2)
+                       changeView(topTileX-2,topTileY-1)
+                       changeView(topTileX-1,topTileY-2)
+                   }
+                   else if (x == 0f){
+                       changeView(x.toInt(),(y-1).toInt())
+                       changeView(x.toInt(),(y+1).toInt())
+                       changeView((x+1).toInt(),y.toInt())
+                   }else if (y == 0f){
+                       changeView((x-1).toInt(),y.toInt())
+                       changeView((x+1).toInt(),y.toInt())
+                       changeView(x.toInt(),(y+1).toInt())
+                   }else if (x == (topTileX - 1).toFloat()){
+                       changeView(x.toInt(),(y-1).toInt())
+                       changeView(x.toInt(),(y+1).toInt())
+                       changeView((x-1).toInt(),y.toInt())
+                   }else if (y == (topTileY - 1).toFloat()){
+                       changeView((x-1).toInt(),y.toInt())
+                       changeView((x+1).toInt(),y.toInt())
+                       changeView(x.toInt(),(y-1).toInt())
+                   }else{
+                       changeView((x-1).toInt(),y.toInt())
+                       changeView((x+1).toInt(),y.toInt())
+                       changeView(x.toInt(),(y-1).toInt())
+                       changeView(x.toInt(),(y+1).toInt())
+                   }
+
                 }
                 l2.addView(tv)
                 tv.setOnClickListener {
@@ -80,7 +144,27 @@ class GameField : AppCompatActivity() {
         }
 
 
+
     }
+    fun changeView(x: Int, y:Int){
+        var tt: TileView = findViewById(ids[x][y])
+        var newIndex = tt.getNewIndex()
+        values[x][y] = newIndex
+        tt.setBackgroundResource(dibujos[newIndex])
+        tt.invalidate()
+    }
+    fun checkIfFinished(){
+        var valor = values[0][0]
+        for (i in 0 .. topTileY){
+            for (j in 0 .. topTileX){
+                if (values[j][i] != valor)
+                    return
+            }
+        }
+
+
+    }
+
     fun miRandom(): Int{
 
         return Random.nextInt(0,topElement)
